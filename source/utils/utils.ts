b04e34a9d9238a9a65ed71f44f37bf506f442555
Sagar Khanal <sagar@optimumfuturist.com>
2022-06-05 15:55:53 +0545

    feat: utils updated with more functions

--- source/utils/utils.ts
+++ source/utils/utils.ts
@@ -12,3 +12,18 @@ export function runOnce(func: Function) {
 
 export const maxDate = (...dates: []) =>
   new Date(Math.max.apply(null, ...dates))
+
+export const objValueSelector = (from: any, ...selectors: any) =>
+  [...selectors].map(s =>
+    s
+      .replace(/\[([^[\]]*)\]/g, '.$1.')
+      .split('.')
+      .filter((t: any) => t !== '')
+      .reduce((prev: any, cur: any) => prev && prev[cur], from),
+  )
+
+// const obj = {
+//   selector: { to: { val: 'val to select' } },
+//   target: [1, 2, { a: 'test' }],
+// }
+// get(obj, 'selector.to.val', 'target[0]', 'target[2].a') // ['val to select', 1, 'test']
